0a5a4b9fffb7afac891aa8871dcd4ce3e193665f
manjil-thapa-patientsky <manjil.thapa@patientsky.com>
2023-03-14 13:31:22 +0100

    wip

--- src/retrospect/retrospectView.tsx
+++ src/retrospect/retrospectView.tsx
@@ -2,7 +2,8 @@ import React, { FunctionComponent, useEffect, useState } from "react"
 import { RetrospectiveInstance } from "@uniscale-sdk/ActorScene-SuperRetro/sdk/FeedbackBros/SuperRetro/SuperRetro/Retrospective/RetrospectiveInstance"
 import { ActorInScene, GatewayRequest, Platform } from "@uniscale-sdk/ActorScene-SuperRetro"
 import { NewRetrospective } from "@uniscale-sdk/ActorScene-SuperRetro/sdk/FeedbackBros/SuperRetro_1_0/Functionality/ServiceToSolution/AgentInPrivate/SuperRetro/ManageRetrospectives_bundle/IWantToBeAbleToAddNewRetrospective_group/NewRetrospective"
-import { randomUUID } from "crypto"
+// import { randomUUID } from "crypto"
+
 export const RetrospectView: FunctionComponent = () => {
   const [retrospectList, setRetrospectList] = useState<RetrospectiveInstance[]>([])
 
@@ -12,12 +13,22 @@ export const RetrospectView: FunctionComponent = () => {
       .withInterceptors((i) =>
         i.interceptPattern("*", async (input, ctx) => {
           const json = GatewayRequest.from(input, ctx)
-          return fetch("http://localhost:5295/retro", { method: "POST", body: JSON.stringify(json) }).then((res) => res.json())
+          return fetch("http://localhost:5295/retro", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(json),
+          }).then((res) => res.json())
         })
       )
       .build()
 
-    const caller = session.asSolution("test").withDataTenant("lol").withTransactionId("id").withLocale("en-GB")
+    const caller = session
+      .asSolution("2caceb43-3989-4740-b4d4-29e4ee5e5d3d")
+      .withDataTenant("2caceb43-3989-4740-b4d4-29e4ee5e5d3d")
+      .withTransactionId("2caceb43-3989-4740-b4d4-29e4ee5e5d3d")
+      .withLocale("en-GB")
 
     const ret = await caller.request(NewRetrospective.with({ name: "Hackathon-March" }))
     console.log(ret)
